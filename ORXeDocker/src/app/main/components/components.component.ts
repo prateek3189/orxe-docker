68591b07ab75208eb723123ba1ca65ee1dde9840
Saurabh Manchanda <smanchanda@tavisca.com>
2019-01-14 13:45:26 +0530

    Updated - Routing

--- ORXeDocker/src/app/main/components/components.component.ts
+++ ORXeDocker/src/app/main/components/components.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ComponentService } from 'src/app/component.service';
 import { ComponentdataService } from 'src/app/componentdata.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-components',
@@ -15,17 +16,36 @@ export class ComponentsComponent {
   public MenuList = [];
   public List = [];
   Name: string;
+  public previousComponent: any;
   @Output() component: EventEmitter<string> = new EventEmitter<string>();
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver, private _componentdata: ComponentdataService) { }
+  constructor(private breakpointObserver: BreakpointObserver, private _componentdata: ComponentdataService, private router: Router) { }
 
   ngOnInit() {
-    this.data = this._componentdata.getComponentData();
-    this.MenuList = this._componentdata.getMenuList();
+    var str = window.location.href;
+    var last = str.substring(str.lastIndexOf("/") + 1, str.length);
+    this.previousComponent = decodeURIComponent(last);
+    if (this.previousComponent != "") {
+      this._componentdata.setComponent(this.previousComponent);
+      this.data = this._componentdata.getComponentData();
+
+      for (let value in this.data) {
+        if (this.data[value].name == this.previousComponent) {
+          this.MoreInfo(this.data[value]);
+          break;
+        }
+      }
+      this.MenuList = this._componentdata.getMenuList();
+      this.router.navigate([this.previousComponent]);
+    }
+    else {
+      this.data = this._componentdata.getComponentData();
+      this.MenuList = this._componentdata.getMenuList();
+    }
   }
 
   MoreInfo(component) {
